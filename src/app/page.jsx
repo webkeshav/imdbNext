e42b6e63580a1cf7ee2b9b5d862018cbbacb282d
Keshav <keshavtomar999@gmail.com>
2023-04-10 01:20:04 +0530

    Fetched data from tmdb api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,25 @@
+import Results from "./components/Results";
 
-export default function Home() {
-  return (
-    <h1>Home</h1>
+const API_KEY = process.env.API_KEY
+
+
+export default async function Home({searchParams}) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(`https://api.themoviedb.org/3/${genre === "fetchTopRated"? "movie/top_rated":"trending/all/week"}?api_key=${API_KEY}&language=en-US&page=1`,
+  {next:{revalidate:10000}});
+
+  if(!res.ok){
+    throw new Error("failed to fetch data");
+  }
+
+  const data = await res.json();
+  const results = data.results;
+  
+ 
+  return ( 
+    <div>
+      <Results results={results} />
+    </div>
   )
 }
+ 
