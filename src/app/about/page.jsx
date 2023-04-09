b514ed1bd55a84e0b8768a127a94a6423def33d7
Keshav <keshavtomar999@gmail.com>
2023-04-09 20:39:55 +0530

    added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,16 @@
+
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>Wlcome to my Movie database website! Here we provide you new recommendations to watch everyday which you can't resist</p>
+      <hr />
+      <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores eum harum molestiae nobis voluptate quasi repellat, dolorem odio fuga nemo ut. Repudiandae qui porro aspernatur, dolores esse omnis tenetur maiores.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore dolor quia obcaecati aspernatur natus asperiores minus at! Fugit nihil ipsa id aliquam expedita alias! Ab corporis quo dolorum quisquam animi magni deleniti, maiores natus corrupti.
+      </p>
+    </div>
+  )
+}
